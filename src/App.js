49d1c51db1a055c9bd6adf86a570625e96845eae
maru <marielacagnoli@gmail.com>
2019-11-20 11:47:01 -0300

    introduccion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  agregarTarea =(evento)=>{
+    if(evento.which === 13){
+      console.log(evento.target.value);
+      this.props.agregar(evento.target.value, this.props.id)
+      evento.target.value = "";
+    }
+    
+  }
+ 
+  render(){
+    const elementosTareas = this.props.tarea.map((tarea)=>{
+    return <h2 key={tarea.id}> {tarea.tarea} </h2>
+    })
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            {this.props.informacion}
+            <br/>
+            <button onClick={this.props.aumentar}>Aumentar</button>
+            <br/>
+            <br/>
+            <button onClick={this.props.disminuir}>Disminuir</button>
+            <br/>
+            <input onKeyPress={this.agregarTarea}/>
+            <br/>
+            {elementosTareas}
+            <br/>
+
+            Edit <code>src/App.js</code> and save to reload.
+            
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Learn React
+          </a>
+          
+        </header>
+      </div>
+    );
+  }
+  }
+  
+//internamente hace una subscripcion y un getState por lo que constantemente
+//en caso de un cambio en el state se actualiza o se vuelve a ejecutar
+
+const mapStateToProps = (state)=>{
+  return {
+    informacion: state.numero.cantidad,
+    tarea: state.tareas,
+    id: state.id 
+  }
+}
+/*const mapDispatchToProps = {
+  aumentar: ()=>{return{type: "AUM"}},
+  dismunuir: ()=>{return{type: "DIS"}}
+}*/
+const mapDispatchToProps = (dispatch) => {
+  return{
+    aumentar: ()=>{dispatch(
+      (dispatch)=>{
+        setTimeout(()=>{ return dispatch({type:"AUM"})},3000);
+       
+      }
+      );},
+    disminuir: ()=>{
+      setTimeout(()=>{dispatch({type:"DIS"});}, 
+      5000);
+      },
+    //disminuir: ()=>{dispatch({type:"DIS"});},
+
+    agregar: (tarea, id)=>{dispatch({type:"ADD",tarea,id});}
+  } 
 }
 
-export default App;
+
+
+//connect nos permite acceder al state y hacer dispatch de actions y actions creator
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,59 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//const state = {cantidad: 2};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const reducerNumero = (state = {cantidad: 2}, action)=>{
+  var nuevoEstado = Object.assign({},state);
+  if(action.type === 'AUM'){
+    console.log("dentro del reducer con el action type === AUM");
+    nuevoEstado.cantidad = state.cantidad + 1;
+  }else if(action.type === 'DIS'){
+    nuevoEstado.cantidad = state.cantidad - 1;
+  }
+  return nuevoEstado;
+}
+
+const reducerTareas = (state = [], action) => {
+  var nuevoEstado = Object.assign({},state);
+  if(action.type === 'ADD'){
+    nuevoEstado = state.concat([{tarea: action.tarea, id: action.id}]);
+    console.log(nuevoEstado);
+    
+    return nuevoEstado;
+  }
+  return state;
+}
+const reducerId = (state = 1, action) => {
+  var nuevoEstado = Object.assign({}, state);
+  if(action.type === 'ADD'){
+    nuevoEstado = state + 1;
+    return nuevoEstado;
+  }
+  
+  return state;
+}
+//combineReducers toma como parametro un objeto javascript con los demas
+//reducers como valores.
+const reducer = combineReducers({
+    numero: reducerNumero,
+    tareas: reducerTareas,
+    id: reducerId
+});
+
+const store = createStore(reducer, applyMiddleware(ReduxThunk));
+
+ReactDOM.render(
+  //I. Implementar el PROVIDER
+  <Provider store={ store }>
+      <App/>
+  </Provider>
+, document.getElementById('root'));
+
+
 serviceWorker.unregister();
+
